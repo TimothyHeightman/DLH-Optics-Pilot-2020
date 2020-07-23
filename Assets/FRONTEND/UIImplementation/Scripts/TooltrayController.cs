72d3018eccb5c3864d10f066f99dd6f58626e365
Luke Murray <lm318@ic.ac.uk>
2020-07-23 12:11:32 +0100

    Initial Commit of UI Build
    Basic skeleton in the frontend folder for further development by Camilla or myself.

---
+++ Assets/FRONTEND/UIImplementation/Scripts/TooltrayController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltrayController : MonoBehaviour
+{
+
+    //Need to expand this game objects height dependent on the number of icons
+    RectTransform tooltrayRect;
+    
+    void Start()
+    {
+        tooltrayRect = gameObject.GetComponent<RectTransform>();
+        tooltrayRect.sizeDelta = new Vector2(tooltrayRect.sizeDelta.x, tooltrayRect.sizeDelta.y + 60);
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/FRONTEND/UIImplementation/Scripts/UIController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+enum Mode
+{
+    Calibrate,
+    Measure,
+    Explore,
+    DataTake
+}
+
+public class UIController : MonoBehaviour
+{
+    public Button calibrate, measure, explore, data;
+    public Camera cam;
+
+    Mode currentMode;
+
+    void Start()
+    {
+        calibrateClick();        //by default launch into this mode (change this for intro mode in the future)
+
+        calibrate.onClick.AddListener(calibrateClick);
+        measure.onClick.AddListener(measureClick);
+        explore.onClick.AddListener(exploreClick);
+        data.onClick.AddListener(dataClick);
+    }
+
+    public void calibrateClick()
+    {
+        currentMode = Mode.Calibrate;
+        cam.transform.position = new Vector3(0, 10, 0);
+        cam.transform.rotation = Quaternion.Euler(90, 0, 0);
+        Debug.Log("calibrate");
+    }
+
+    public void measureClick()
+    {
+        currentMode = Mode.Measure;
+        cam.transform.position = new Vector3(0, 0, -15);
+        cam.transform.rotation = Quaternion.identity;
+        Debug.Log("Measure");
+    }
+
+    public void exploreClick()
+    {
+        currentMode = Mode.Explore;
+        cam.transform.position = new Vector3(0, 0, -10);
+        Debug.Log("Explore");
+    }
+
+    public void dataClick()
+    {
+        currentMode = Mode.DataTake;
+        cam.transform.position = new Vector3(0, 0, -5);
+        Debug.Log("Data");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
